f61d31b31ed75ca1b3e7450f203852dfbb53e1fc
Håkan Jansson <hkj@cypress.com>
2016-03-06 17:41:30 +0100

    Added custom service and characteristic

--- Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/SampleGattAttributes.java
@@ -24,15 +24,18 @@ import java.util.HashMap;
 public class SampleGattAttributes {
     private static HashMap<String, String> attributes = new HashMap();
     public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
+    public static String LED_STATE = "00000001-0001-0008-0000-0805f9b34fb0";
     public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
 
     static {
         // Sample Services.
         attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
         attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+        attributes.put("00000000-0000-1000-8000-00805f9b34fb", "Custom Service: LedService");
         // Sample Characteristics.
         attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
         attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+        attributes.put(LED_STATE, "Custom Characteristic: LedState");
     }
 
     public static String lookup(String uuid, String defaultName) {
